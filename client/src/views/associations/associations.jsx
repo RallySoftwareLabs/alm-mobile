945b15115d02be615f9aa5721933462da1537f42
Matt Parrish <mparrish@rallydev.com>
2014-02-21 13:41:29 -0700

    Fixed button naming and icon on association page for user stories

--- client/src/views/associations/associations.jsx
+++ client/src/views/associations/associations.jsx
@@ -14,7 +14,7 @@ define(function() {
           <h4>
             <span className={this._getIconCls()}/>
             {association}
-            <button className="btn btn-primary add-button" onClick={ this._onAddClick }>+ Add { this._singularize(association) }</button>
+            <button className="btn btn-primary add-button" onClick={ this._onAddClick }>+ Add { _.singularize(association) }</button>
           </h4>
           <div className="listing">
             <ListView
@@ -28,15 +28,11 @@ define(function() {
     },
 
     _getIconCls: function() {
-      var association = this.props.association;
-      return 'picto icon-' + association.toLowerCase().slice(0, association.length - 1);
-    },
-
-    _singularize: function(association) {
-      if (association === 'Children') {
-        return 'Child';
+      var association = _.singularize(this.props.association);
+      if (association === 'User Story') {
+        association = 'Story';
       }
-      return _.singularize(association);
+      return 'picto icon-' + association.toLowerCase().replace(/\s/g, '-');
     },
 
     _onAddClick: function() {
