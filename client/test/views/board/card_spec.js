f6ea505ce75bf999c2fa5802b8d31e75bc26f235
Matt Parrish <mparrish@rallydev.com>
2014-04-01 12:39:46 -0600

    Adding tests for views/board/board

---
+++ client/test/views/board/card_spec.js
@@ -0,0 +1,41 @@
+var React = require('react');
+var ReactTestUtils = React.addons.TestUtils;
+var app = require('application');
+var CardView = require('views/board/card');
+var UserStory = require('models/user_story');
+
+describe('views/board/card', function() {
+    it('should publish event on card click', function() {
+        var userstory = new UserStory({
+            FormattedID: 'S12345'
+        });
+        var view = ReactTestUtils.renderIntoDocument(CardView({
+            model: userstory
+        }));
+        
+        var eventSpy = this.spy();
+        this.subscribeEvent('cardclick', eventSpy);
+
+        ReactTestUtils.Simulate.click(view.getDOMNode());
+
+        expect(eventSpy).to.have.been.calledOnce;
+        expect(eventSpy).to.have.been.calledWith(view, userstory);
+    });
+
+    it('should record action on card click', function() {
+        var userstory = new UserStory({
+            FormattedID: 'S12345'
+        });
+        var view = ReactTestUtils.renderIntoDocument(CardView({
+            model: userstory
+        }));
+        
+        ReactTestUtils.Simulate.click(view.getDOMNode());
+
+        expect(app.aggregator.recordAction).to.have.been.calledOnce;
+        expect(app.aggregator.recordAction).to.have.been.calledWith({
+            component: view,
+            description: "clicked card"
+        });
+    });
+});
