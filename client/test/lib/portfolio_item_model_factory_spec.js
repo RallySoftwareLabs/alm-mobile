c46caf1ede73d5466db7ceb3386b055642b09ec2
Matt Parrish <mparrish@rallydev.com>
2014-04-01 14:14:26 -0600

    Caching results from PortfolioItemModelFactory and added tests

---
+++ client/test/lib/portfolio_item_model_factory_spec.js
@@ -0,0 +1,69 @@
+var PortfolioItemModelFactory = require('lib/portfolio_item_model_factory');
+var TypeDefinitions = require('collections/type_definitions');
+var TypeDefinition = require('models/type_definition');
+
+describe('lib/portfolio_item_model_factory', function() {
+
+    beforeEach(function() {
+        PortfolioItemModelFactory.clear();
+    });
+    
+    describe('#getCollection', function() {
+        it('should build the collections correctly', function(done) {
+            var fetchStub = this.stubCollectionFetch(
+                TypeDefinitions, 'fetchAllPages', [
+                    {
+                        Ordinal: 0,
+                        TypePath: 'PortfolioItem/Feature',
+                        Name: 'Feature'
+                    },
+                    {
+                        Ordinal: 1,
+                        TypePath: 'PortfolioItem/Initiative',
+                        Name: 'Initiative'
+                    }
+                ]
+            );
+
+            PortfolioItemModelFactory.getCollection(0).then(function(featureModel) {
+                expect(fetchStub).to.have.been.calledOnce;
+                expect(featureModel.prototype.typePath).to.equal('PortfolioItem/Feature');
+                expect(featureModel.prototype.url).to.include('portfolioitem/feature');
+                expect(featureModel.prototype.name).to.equal('Feature');
+                expect(featureModel.prototype.model.prototype.typePath).to.equal('PortfolioItem/Feature');
+                expect(featureModel.prototype.model.prototype.urlRoot).to.include('portfolioitem/feature');
+                expect(featureModel.prototype.model.prototype.name).to.equal('Feature');
+                done();
+            });
+        });
+
+        it('should cache results', function(done) {
+            var fetchStub = this.stubCollectionFetch(
+                TypeDefinitions, 'fetchAllPages', [
+                    {
+                        Ordinal: 0,
+                        TypePath: 'PortfolioItem/Feature',
+                        Name: 'Feature'
+                    },
+                    {
+                        Ordinal: 1,
+                        TypePath: 'PortfolioItem/Initiative',
+                        Name: 'Initiative'
+                    }
+                ]
+            );
+
+            PortfolioItemModelFactory.getCollection(0).then(function(featureModel) {
+                expect(fetchStub).to.have.been.calledOnce;
+                fetchStub.reset();
+            }).then(function() {
+                PortfolioItemModelFactory.getCollection(1).then(function(initiativeModel) {
+                    expect(fetchStub).not.to.have.been.called;
+                    expect(initiativeModel.prototype.typePath).to.equal('PortfolioItem/Initiative');
+                    done();
+                });
+            });
+        });
+    });
+    
+});
