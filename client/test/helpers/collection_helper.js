cb2d95fbcacfcc3c820921cd26dfaf2b88591a0a
Matt Parrish <mparrish@rallydev.com>
2014-04-23 14:15:41 -0600

    Adding test helper functions

--- client/test/helpers/collection_helper.js
+++ client/test/helpers/collection_helper.js
@@ -1,10 +1,13 @@
 var $ = require('jquery');
 
 beforeEach(function() {
-    this.stubCollectionFetch = function(collection, property, items) {
-        return this.stub(collection.prototype, property, function() {
+    this.stubCollectionFetch = function(collection, property, items, fn) {
+        return this.stub(collection.prototype, property, function(opts) {
             var d = $.Deferred();
             this.add(items);
+            if (_.isFunction(fn)) {
+                fn.call(this, opts);
+            }
             d.resolve(this);
             return d.promise();
         });
---
+++ client/test/helpers/mock_view_helper.js
@@ -0,0 +1,18 @@
+var Messageable = require('lib/messageable');
+
+beforeEach(function() {
+    this.stubViewRender = function(Controller, config) {
+        var View = function() {};
+        _.extend(View.prototype, Messageable);
+        _.extend(View.prototype, {
+            setState: this.stub()
+        });
+        _.extend(View.prototype, config);
+        var stub = this.stub(Controller.prototype, 'renderReactComponent', function() {
+            d = $.Deferred();
+            d.resolve(new View());
+            return d.promise();
+        });
+        return stub;
+    };
+});
